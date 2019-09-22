d0eace2b7dc7dacbd9bfaa98b68b0d269e19c4c1
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-09-22 20:12:54 +0300

    SQLiteOpenHelper Added - Part 2

--- ud851-Exercises-student/Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ ud851-Exercises-student/Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -2,6 +2,8 @@ package com.example.android.waitlist.data;
 
 import android.provider.BaseColumns;
 
+// 1. Adım , Veritabanın temel tablo ismi ve kolon isimlerini oluşturan yapının hazırlanması.
+
 public class WaitlistContract {
 
     public static final class WaitlistEntry implements BaseColumns {
